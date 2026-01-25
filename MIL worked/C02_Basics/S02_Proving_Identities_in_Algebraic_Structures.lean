8401f9e45e1eefc2079e78755062a6c3dd1e5115
Augustr-pro <BUS_ARhoda26@outlook.com>
2026-01-25 15:21:43 -0500

    checkpoint

--- MIL worked/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean
+++ MIL worked/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean
@@ -166,11 +166,18 @@ theorem alt_self_sub
   by
     exact sub_self a
 
-theorem one_add_one_eq_two : 1 + 1 = (2 : R) := by
-  norm_num
+theorem one_add_one_eq_two
+: 1 + 1 = (2 : R) :=
+  by
+    norm_num
 
-theorem two_mul (a : R) : 2 * a = a + a := by
-  sorry
+theorem two_mul
+(a : R)
+: 2 * a = a + a :=
+  by
+    rw [← one_add_one_eq_two,
+        add_mul,
+        one_mul]
 
 end MyRing
 
